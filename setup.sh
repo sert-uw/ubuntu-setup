074d66369a1d77a8c93fae64c4d03a5428299e5d
sert <demo.sert@gmail.com>
2015-07-06 19:36:48 +0900

    add install zsh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install tmux
+sudo apt-get install zsh
 
+zsh_path=`which zsh`
+echo "zsh path = "${zsh_path}
+chsh
+
+sudo apt-get install tmux
 sudo apt-get install vim
 
 sh git-install.sh
+sh dotfiles.sh
