5dc8ad1211b31de4f87c0a21f0b05a519af678f6
sert <demo.sert@gmail.com>
2015-07-06 18:48:53 +0900

    add sudo

--- git-install.sh
+++ git-install.sh
@@ -1,7 +1,7 @@
-add-apt-repository ppa:git-core/ppa
-apt-get update
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
 
-apt-get install git
-apt-get upgrade
+sudo apt-get install git
+sudo apt-get upgrade
 
 git config --global core.editor 'vim -c "set fenc=utf-8"'
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
-apt-get install vim
+sudo apt-get install vim
 
 sh git-install.sh
