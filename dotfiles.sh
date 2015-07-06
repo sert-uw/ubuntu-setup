32a14d556bfc4b3c10ba9ef50e69092c82d94913
sert <demo.sert@gmail.com>
2015-07-06 20:34:37 +0900

    add dotfiles.sh and vim-setup.sh

---
+++ dotfiles.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/sert-uw/dotfiles.git ~/dotfiles
+
+cp ~/dotfiles/.gitignore ~/
+cp ~/dotfiles/.tigrc ~/
+cp ~/dotfiles/.tmux.conf ~/
+cp ~/dotfiles/.vimrc ~/
+cp ~/dotfiles/.zshrc ~/
---
+++ vim-setup.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.vim/bundle
+sudo chmod 777 ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
